644d9d12207753817f1209bf84f12c67974fd854
xkadzama <ya.amerika-2015@yandex.ru>
2020-12-06 02:00:03 +0300

    'fixed_bag_db.create_all()'

--- main_app.py
+++ main_app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from forms import RegistrationForm, LoginForm
 from models import db
-from flask_bcrypt import Bcrypt
 from models import *
+from flask_bcrypt import Bcrypt
 # from flask_migrate import Migrate
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'si'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-db = db.init_app(app)
+db.init_app(app)
 bcrypt = Bcrypt(app)
 # migrate = Migrate(app, db)
 
@@ -36,10 +38,11 @@ base = [
 
 @app.route('/')
 def index():
+    db.create_all()
     
     return render_template('base.html', data=base)
 
-
+ 
 
 
 
@@ -51,10 +54,10 @@ def index():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password )
-        db.session.add(user)
-        db.session.commit()
+        # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # user = User(username=form.username.data, email=form.email.data, password=hashed_password )
+        # db.session.add(user)
+        # db.session.commit()
         flash('Аккаунт создан', 'success')
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
